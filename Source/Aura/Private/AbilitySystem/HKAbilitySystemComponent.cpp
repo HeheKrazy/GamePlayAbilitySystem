fb1f3f607bf259357adb820ea705dc1ba285fd7e
HeheKrazy <AdamSabo2750@Gmail.com>
2024-06-09 09:52:34 -0400

    Get All Asset Tags

--- Source/Aura/Private/AbilitySystem/HKAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/HKAbilitySystemComponent.cpp
@@ -10,5 +10,13 @@ void UHKAbilitySystemComponent::AbilityActorInfoSet()
 
 void UHKAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("Effect Actor"));
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		//TODO: Boradcast the tag to the Widget Controller
+
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+	}
 }
