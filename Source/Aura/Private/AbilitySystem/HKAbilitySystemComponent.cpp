3ae534c96745fcb9c1e250e01b45244daba22198
HeheKrazy <AdamSabo2750@Gmail.com>
2024-06-18 22:02:04 -0400

    Native Gameplay Tags

--- Source/Aura/Private/AbilitySystem/HKAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/HKAbilitySystemComponent.cpp
@@ -7,10 +7,6 @@
 void UHKAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UHKAbilitySystemComponent::EffectApplied);
-
-	const FHKGameplayTags& GameplayTags = FHKGameplayTags::Get();
-	//GameplayTags.Attributes_Secondary_Armor.ToString()
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Orange, FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString()));
 }
 
 void UHKAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
