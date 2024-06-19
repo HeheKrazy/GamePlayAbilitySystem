573d9f269c990092555d844cf3640b7243e1605b
HeheKrazy <AdamSabo2750@Gmail.com>
2024-06-19 17:13:16 -0400

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright HeheKrazy
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
---
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,21 @@
+// Copyright HeheKrazy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/HKWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UHKWidgetController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BindCallbacksToDependencies() override;
+	virtual void BroadcastInitialValues() override;
+	
+};
