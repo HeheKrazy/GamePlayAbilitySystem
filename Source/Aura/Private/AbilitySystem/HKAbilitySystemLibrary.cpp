369dafeb93d2e0a90774a0a1d8489d0cb157fb9d
HeheKrazy <AdamSabo2750@Gmail.com>
2024-06-19 17:36:43 -0400

    HK AbilitySystem Library

---
+++ Source/Aura/Private/AbilitySystem/HKAbilitySystemLibrary.cpp
@@ -0,0 +1,25 @@
+// Copyright HeheKrazy
+
+
+#include "AbilitySystem/HKAbilitySystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/HUD/HKHUD.h"
+#include "UI/WidgetController/HKWidgetController.h"
+#include "Player/MyPlayerState.h"
+
+UOverlayWidgetController* UHKAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (AHKHUD* HKHUD = Cast<AHKHUD>(PC->GetHUD()))
+		{
+			AMyPlayerState* PS = PC->GetPlayerState<AMyPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+
+			return HKHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
---
+++ Source/Aura/Public/AbilitySystem/HKAbilitySystemLibrary.h
@@ -0,0 +1,24 @@
+// Copyright HeheKrazy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "HKAbilitySystemLibrary.generated.h"
+
+class UOverlayWidgetController;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UHKAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintPure, Category = "HKAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+	
+};
