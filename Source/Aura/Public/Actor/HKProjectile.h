2849c8252d977a9a4e0efddeb286daff758a4b5b
HeheKrazy <AdamSabo2750@Gmail.com>
2024-06-21 14:29:09 -0400

    HKProjectile

---
+++ Source/Aura/Public/Actor/HKProjectile.h
@@ -0,0 +1,31 @@
+// Copyright HeheKrazy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HKProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class AURA_API AHKProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AHKProjectile();
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
