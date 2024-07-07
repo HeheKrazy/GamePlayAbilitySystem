d392e1ca73b90706f2ff18745ed1bed577c39609
HeheKrazy <AdamSabo2750@Gmail.com>
2024-07-07 11:14:11 -0400

    Ability Damage

--- Source/Aura/Private/AbilitySystem/Abilities/HKProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/HKProjectileSpell.cpp
@@ -39,8 +39,11 @@ void UHKProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation
 		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
 
 		FHKGameplayTags GameplayTags = FHKGameplayTags::Get();
-		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, 50.f);
+		const float ScaledDamage = Damage.GetValueAtLevel(GetAbilityLevel());
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, FString::Printf(TEXT("FireBolt Damage : %f"), ScaledDamage));
+		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, ScaledDamage);
 		Projectile->DamageEffectSpecHandle = SpecHandle;
+
 		Projectile->FinishSpawning(SpawnTransform);
 	}
 }
