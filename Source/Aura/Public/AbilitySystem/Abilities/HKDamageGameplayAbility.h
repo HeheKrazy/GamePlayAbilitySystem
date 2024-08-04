c81a8f18accbdf14d1f93db4493499c2618b00a1
HeheKrazy <AdamSabo2750@Gmail.com>
2024-08-04 10:25:30 -0400

    Playing the Ranged Attack Montage

--- Source/Aura/Public/AbilitySystem/Abilities/HKDamageGameplayAbility.h
+++ Source/Aura/Public/AbilitySystem/Abilities/HKDamageGameplayAbility.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystem/Abilities/HKGameplayAbility.h"
+#include "Interaction/CombatInterface.h"
 #include "HKDamageGameplayAbility.generated.h"
 
 /**
@@ -24,5 +25,8 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	TMap<FGameplayTag, FScalableFloat> DamageTypes;
+
+	UFUNCTION(BlueprintPure)
+	FTaggedMontage GetRandomTaggedMontageFromArray(const TArray<FTaggedMontage>& TaggedMontages) const;
 
 };
