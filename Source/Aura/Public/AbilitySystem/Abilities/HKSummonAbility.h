30a7fce864e9f9a5100d42fef744642b217a91b2
HeheKrazy <AdamSabo2750@Gmail.com>
2024-08-05 12:47:19 -0400

    Shaman Summon Locations

---
+++ Source/Aura/Public/AbilitySystem/Abilities/HKSummonAbility.h
@@ -0,0 +1,36 @@
+// Copyright HeheKrazy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/HKGameplayAbility.h"
+#include "HKSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UHKSummonAbility : public UHKGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	int32 NumMinionsToSpawn = 5;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MinSpawnDistance = 150.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MaxSpawnDistance = 400.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float SpawnSpread = 90.f;
+};
