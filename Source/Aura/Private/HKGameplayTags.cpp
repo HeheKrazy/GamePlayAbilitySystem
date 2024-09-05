60c4110f75f710c6e9fb800aa000a170e2bc40fb
HeheKrazy <AdamSabo2750@Gmail.com>
2024-09-05 18:35:59 -0400

    Equipped Spell Row

--- Source/Aura/Private/HKGameplayTags.cpp
+++ Source/Aura/Private/HKGameplayTags.cpp
@@ -100,6 +100,14 @@ void FHKGameplayTags::InitializeNativeGameplayTags()
 		FName("InputTag.4"),
 		FString("Input Tag for 4 key"));
 
+	GameplayTags.InputTag_Passive_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Passive.1"),
+		FString("Input Tag Passive Ability 1"));
+
+	GameplayTags.InputTag_Passive_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("InputTag.Passive.2"),
+		FString("Input Tag Passive Ability 2"));
+
 	/* Damage */
 	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Damage"),
